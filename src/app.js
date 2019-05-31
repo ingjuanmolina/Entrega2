e8afa22f4e8737a23bf0e4ef6b7b06433e6a17d0
ingjuanmolina <ing.jcmolina@gmail.com>
2019-05-31 08:42:32 -0500

    First commit with reading course list function

---
+++ src/app.js
@@ -0,0 +1,57 @@
+//Entry App Point
+
+//Variables used to manage express
+const express = require('express');
+const app = express();
+
+const path = require('path'); //Variable to handle path package features
+
+const hbs = require('hbs'); //Variable to handle hbs package features
+
+require('./helpers');
+
+//Used to read data from POST method
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.urlencoded({extended: false}));
+
+//Public directory config
+const publicDirectory = path.join(__dirname,'../public');
+app.use(express.static(publicDirectory));
+
+//Partials directory config
+const partialsDirectory = path.join(__dirname, '../partials');
+hbs.registerPartials(partialsDirectory);
+
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Inicio'
+    });
+});
+
+app.get('/coordinador', (req, res) => {
+    res.render('coordinador', {
+        title: 'Administrar Cursos'
+    });
+});
+
+app.get('/cursos', (req, res) => {
+    res.render('listarcursos', {
+        title: 'Cursos'
+    });
+});
+
+app.post('/crear', (req, res) => {
+    console.log(req.body);
+    res.render('listarcursos', {
+        title: 'Cursos'
+    });
+})
+
+console.log(__dirname);
+
+//Opens port 3000 listening
+app.listen(3000, () => {
+    console.log('Escuchando en el puerto 3000');
+});
---
+++ src/helpers.js
@@ -0,0 +1,54 @@
+const hbs = require('hbs'); //Variable to handle hbs package features
+
+hbs.registerHelper('listar', () => {
+    let courseList = require('./data.json');
+    
+    let output = ''; //output from json object
+
+    courseList.forEach(course => {
+        output = output +
+    `<tr>
+        <td><a href="/listar?id=${course.id}">${course.id}</a></td>
+        <td>${course.name}</td>
+        <td>${course.description}</td>
+        <td>${course.value}</td>
+    </tr>`
+    });
+
+    console.log(output);
+    return output;
+});//End of registerHelper listar
+
+hbs.registerHelper('listarcollapse', () => {
+    let courseList = require('./data.json');
+    
+    let output = ''; //output from json object
+
+    let i = 1
+
+    courseList.forEach(course => {
+        output = output +
+    `
+    <div class="accordion" id="accordionExample">
+        <div class="card">
+            <div class="card-header" id="heading${i}">
+            <h2 class="mb-0">
+                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="true" aria-controls="collapseOne">
+                Curso: ${course.name} - ${course.description}. $ ${course.value}.
+                </button>
+            </h2>
+            </div>
+
+            <div id="collapse${i}" class="collapse" aria-labelledby="heading${i}" aria-expanded="false" data-parent="#accordionExample">
+                <div class="card-body">
+                    ${course.description}. Curso de modalidad ${course.mode}, con una duración de ${course.hours} horas.
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+        i++;
+    });
+
+    return output;
+});//End of registerHelper listarcollapse
